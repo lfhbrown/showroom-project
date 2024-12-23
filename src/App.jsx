29a3816c2aa5534d26ed8fe54a3f92c845c9987d
6Weapons <89061083+lfhbrown@users.noreply.github.com>
2024-12-22 19:56:11 -0800

    Upgrade to styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-// import { useState } from "react";
-import "./App.css";
+import { useState } from "react";
+import { styled } from "styled-components";
 import Navbar from "./components/Fixed Components/Navbar";
 import Footer from "./components/Fixed Components/Footer";
 import Signup from "./components/Modal/Feedback";
